669a4f361297188cc6ac63d9f15d9e3edaf4982a
Cedric Hazoume <cedric.hazoume@webbfontaine.com>
2022-10-27 14:31:57 +0100

    adding the context

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Store from './pages/Store';
 import Success from './pages/Success';
 import Cancel from './pages/Cancel';
+import CartProvider from './CartContext';
 
 function App() {
   return (
-    <Container>
-      <NavbarComponent></NavbarComponent>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Store/>}></Route>
-          <Route path="success" element={<Success/>}></Route>
-          <Route path="cancel" element={<Cancel/>}></Route>
-        </Routes>
-      </BrowserRouter>
-    </Container>
+    <CartProvider>
+      <Container>
+        <NavbarComponent></NavbarComponent>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Store/>}></Route>
+            <Route path="success" element={<Success/>}></Route>
+            <Route path="cancel" element={<Cancel/>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </CartProvider>
   );
 }
 
--- src/components/ProductCards.js
+++ src/components/ProductCards.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Card, Button, Form, Row, Col } from "react-bootstrap";
+import { CartContext } from "../CartContext";
+import { useContext } from "react";
 
 const ProductCards = ({product}) => {
   return (
